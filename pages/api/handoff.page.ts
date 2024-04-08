46a8fa475eab1b1f14fb9061631596a1f8546d21
Daniel Bisgrove <daniel@bizz-websites.com>
2024-04-08 17:01:29 -0400

    fixup! Redirect user back to old MPDx when early aopter turned off

--- pages/api/handoff.page.ts
+++ pages/api/handoff.page.ts
@@ -36,13 +36,11 @@ export const returnRedirectUrl = async (req: NextApiRequest) => {
     }
     const userId = req.query.userId || jwtToken.userID;
 
-    let hypertextTransferProtocol = 'https';
+    let protocol = 'https';
     if (process.env.REWRITE_DOMAIN?.includes('localhost')) {
-      hypertextTransferProtocol = 'http';
+      protocol = 'http';
     }
-    const url = new URL(
-      `${hypertextTransferProtocol}://${process.env.REWRITE_DOMAIN}/handoff`,
-    );
+    const url = new URL(`${protocol}://${process.env.REWRITE_DOMAIN}/handoff`);
 
     url.searchParams.append('accessToken', jwtToken.apiToken);
     url.searchParams.append('accountListId', defaultAccountID.toString());
